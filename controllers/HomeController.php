a767b621942998d82950b2f4613381a67c4fdfc2
JoelDeoKL <joeldeocm88@gmail.com>
2021-08-05 15:49:02 +0200

    Reforme du systeme de chargement des fichiers

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,17 +1,12 @@
 <?php
 
 //require_once 'ressources/inc/navbar.php';
-require_once("entities/Citations.php");
-require_once("models/CitationsModel.php");
+//require_once("entities/Citations.php");
+//require_once("models/CitationsModel.php");
 
 class HomeController extends x_Controller{
 
     public function index(){
-
-        $citations = new Citations(null, null, null, null, null, null);
-        $data = new CitationsModel();
-        $resultats = $data->affiche($citations);
-
-        $this->load->view("home", compact("resultats"));
+        $this->load->view("home");
     }
 }
