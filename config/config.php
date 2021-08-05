e01e7287167d660ea1eb48bd4b254152fc68c316
JoelDeoKL <joeldeocm88@gmail.com>
2021-08-05 15:45:00 +0200

    Mise en place d'une connexion a la base de donnees

--- config/config.php
+++ config/config.php
@@ -10,5 +10,5 @@ session_start();
 if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
 if (!defined('DB_USER')) define('DB_USER', "root");
 if (!defined('DB_PWD')) define('DB_PWD', "");
-if (!defined('DB_NAME')) define('DB_NAME', "citations");
+if (!defined('DB_NAME')) define('DB_NAME', "ressources");
 if (!defined('DSN')) define('DSN', 'mysql:host='.DB_SERVER.'; dbname='.DB_NAME);
