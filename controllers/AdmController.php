7973be8376dc5b88f6ca5bc855f9311ed3aff483
JoelDeoKL <joeldeocm88@gmail.com>
2021-08-05 16:27:44 +0200

    Finalisation des fonctions relative a la connexion de l'admnistrateur

--- controllers/AdmController.php
+++ controllers/AdmController.php
@@ -4,4 +4,31 @@
 class AdmController extends x_Controller
 {
 
+    public function inscrire(){
+        if(isset($_POST["inscrire"])){
+            $this->ajoutAdm();
+        }
+        $this->load->view("addAdm");
+    }
+
+    public function ajoutAdm(){
+        extract($_POST);
+
+        $mdp = password_hash($password, PASSWORD_BCRYPT);
+
+        $adm = new Adm(null, $pseudo, $email, $mdp);
+        $connexion = new AddModel();
+
+        if($connexion->check($adm)){
+            header("Location : index.php?kay=x-users.inscription");
+        }else{
+            if ($password == $confirm){
+                $connexion->inscription($adm);
+            }else{
+                echo 'Veuillez utiliser le meme mot de passe.';
+            }
+        }
+    }
+
+
 }
